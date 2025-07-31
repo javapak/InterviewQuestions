3e1d75422e5544d6eb9732eb662ae290b51d56cd
James Pakis <japakis@memphis.edu>
2025-07-31 18:56:56 -0500

    have to go grab some food and run some errands so i will be coming back to this and submitting tomorrow!

--- response/TotalCompensation.ts
+++ response/TotalCompensation.ts
@@ -20,68 +20,19 @@ function stripJsonC(): CombinedData {
 
 }
 
-function totalHrsFromTp(timePunch: TimePunch): number {
-    const startDate = new Date(timePunch.start);
-    const endDate = new Date(timePunch.end);
-    /* 
-    getTime() returns a milisecond value of time elapsed from Jan 1, 1970 UTC
-    division operations derived from standard unit conversion:
-    1000 ms in 1s, 60 seconds in 1 minute, 60 minutes in 1 hour... 
-    */
-    return ((((endDate.getTime() - startDate.getTime()) / 1000) / 60) / 60); 
-    // I guess you could simplify this by: 1000 * 60 * 60 = 3600000 but I find that to be less readable
+
+
+function enterCalculationStart() {
+    const root = stripJsonC();
+    root.employeeData.forEach((record) => {
+        const jobRecordsMap: Record<string, {normal: number, overtime: number, doubletime: number}> = {};
+        record.timePunch.forEach((timePunch) => {
+
+        });
+    });
+
+
 }
-
-function totalHoursCountTrackOvertime(timePunchCollection: [TimePunch]) {
-    var overtimeHours = 0;
-    var doubleTimeHours = 0;
-    var normalHours = 0;
-    var total = 0;
-    const nonOverTimeValues: Record<string, number> = {} // At or  below 40 hour maximum.
-    const timeAndAHalfValues: Record<string, number> = {} // At or below 48 hour maximum.
-    const doubleTimeValues: Record<string, number> = {} // Exceeds 48 hour maximum.
-
-    timePunchCollection.forEach((timePunchEntry) => {
-        if (!nonOverTimeValues[timePunchEntry.job]) { 
-            // we can go ahead and initialize for all if we find that a specific key for a job doesn't exist to avoid errors.
-            nonOverTimeValues[timePunchEntry.job] = 0;
-            timeAndAHalfValues[timePunchEntry.job] = 0;
-            doubleTimeValues[timePunchEntry.job] = 0
-        }
- 
-        const currentCardHours = totalHrsFromTp(timePunchEntry);
-        total += currentCardHours;
-        
-        if (total <= 40) {
-            if ((normalHours + currentCardHours ) <= 40) {
-            normalHours += currentCardHours;
-            nonOverTimeValues[timePunchEntry.job] += currentCardHours;
-            }
-            else {
-                const difference = (normalHours + currentCardHours) - 40
-                overtimeHours += difference;
-                normalHours += currentCardHours - difference;
-                nonOverTimeValues[timePunchEntry.job] += normalHours;
-                timeAndAHalfValues[timePunchEntry.job] += difference;
-
-            }
-        }
-
-        
-        else if (total > 40 && total <= 48) {
-            timeAndAHalfValues[timePunchEntry.job] += currentCardHours;
-            overtimeHours += currentCardHours;
-        }
-
-        else {
-            doubleTimeValues[timePunchEntry.job] += currentCardHours;
-
-        }
-    }
-    )
-}
-
-
 
 function createRecordForJob(existingTotals: 
 Record<string, {normal: number, overtime: number, doubletime: number}>,
@@ -97,17 +48,18 @@ jobAlias: string
 }
 
 function calculateNormalHoursAndOverSpill(currentTotal: number, timePunchHours: number) {
+    // This one is actually kind of tricky!!! There is probably a more optimal way to write this admittedly. The less cases of overspill the less confusing though.
     const classifiedHours = {normal: 0, overtime: 0, doubletime: 0 }
     const difference = (currentTotal + timePunchHours) - 40
     if (currentTotal + timePunchHours > 40) {
         if (difference > 8) {
-            classifiedHours.overtime = 8 - difference;
-            classifiedHours.doubletime = difference - 8;
-            classifiedHours.normal = difference - classifiedHours.doubletime - classifiedHours.overtime
+            classifiedHours.overtime = 8;
+            classifiedHours.normal = 40 - currentTotal;
+            classifiedHours.doubletime = timePunchHours - classifiedHours.normal - classifiedHours.doubletime;
             return classifiedHours;
         }
-        classifiedHours.overtime = difference;
-        classifiedHours.normal = timePunchHours - difference;
+        classifiedHours.normal = 40 - currentTotal;
+        classifiedHours.overtime = timePunchHours - classifiedHours.normal;
     }
     return classifiedHours;
 }
@@ -115,7 +67,7 @@ function calculateNormalHoursAndOverSpill(currentTotal: number, timePunchHours: 
 function calculateOvertimeHoursAndOverSpill(currentTotal: number, punchTimeHours: number) {
     // this function is effectively the same as the above but since we know we have gone over 40 total we skip any normal hr calc...
     const classifiedHours = {normal: 0, overtime: 0, doubletime: 0 } // just for consistency i am still populating the key for normal.
-    const difference = (currentTotal + punchTimeHours) - 48
+    const difference = (currentTotal + punchTimeHours) - 48;
     if ((currentTotal + punchTimeHours) > 48) {
         classifiedHours.doubletime = difference;
         classifiedHours.overtime = punchTimeHours - difference;
